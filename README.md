949058687fcd46ad223aa34f770fd65085d09f64
ry00001 <dar.gargano@gmail.com>
2017-07-20 18:28:01 +0100

    Replacer.py added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Utilities
+These are some random bits of code I have cooked up in [Python](http://python.org).  
+These shouldn't be taken seriously.
---
+++ replacer.py
@@ -0,0 +1,11 @@
+import sys, string, random
+a = 0
+try:
+    while True:
+        stdin = sys.stdin.readline()
+        stdin.replace("a", random.choice(string.letters))
+        print(stdin)
+except KeyboardInterrupt:
+    sys.stdout.flush()
+    pass
+print a
